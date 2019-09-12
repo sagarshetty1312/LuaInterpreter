fd55122051c33be1e703047586a883b5c175c240
sagarshetty1312 <Sagar@SAGAR>
2019-09-12 03:56:37 -0400

    Other tokens

--- Compiler/src/cop5556fa19/Scanner.java
+++ Compiler/src/cop5556fa19/Scanner.java
@@ -24,12 +24,13 @@ import java.io.Reader;
 public class Scanner {
 
 	public enum State {
-		START, HAVE_EQ, HAVE_REL_GT, HAVE_REL_LT, IN_NUMLIT, IN_IDENT
+		START, HAVE_EQ, HAVE_DIV, HAVE_XOR, HAVE_REL_LT, HAVE_REL_GT, HAVE_COLON, HAVE_DOT, IN_NUMLIT, IN_IDENT
 	}
 	Reader r;
 
 	public Scanner(Reader r) throws IOException {
 		this.r = r;
+		getChar();
 	}
 
 
@@ -39,8 +40,12 @@ public class Scanner {
 	int ch;
 
 	void getChar() throws IOException {
-		//read next char
-		//update currPos and currLine
+		ch = r.read();
+		currPos++;
+		currLine++;
+		//TBD!
+		//update currPos and currLine after checking for line end
+		//File completed
 	}
 
 	@SuppressWarnings("serial")
@@ -58,51 +63,43 @@ public class Scanner {
 		int pos = -1;
 		int line = -1;
 		State state = State.START;
-		if(nextchar == -2) {
-			ch = r.read();			
-		}
-		else {
-			ch = nextchar;
-			nextchar = -2;
-		}
 
 
 		while (t==null) {
+			pos = currPos; //Line and column calculation
+			line = currLine;
+			
 			switch (state) {
 			case START: {
 				// skip white space
 				//some sort of for loop
-				pos = currPos;
-				line = currLine;
 				switch (ch) {
 				case '+': {t = new Token(OP_PLUS, "+", pos, line);getChar();}break;
 				case '-': {t = new Token(OP_MINUS, "-", pos, line);getChar();}break;
 				case '*': {t = new Token(OP_TIMES, "*", pos, line);getChar();}break;
-				//case '/': {state = State.HAVE_EQ; getChar();}break; //OP_DIV
+				case '/': {state = State.HAVE_DIV; getChar();}break;
 				case '%': {t = new Token(OP_MOD, "%", pos, line);getChar();}break;
 				case '^': {t = new Token(OP_POW, "^", pos, line);getChar();}break;
 				case '#': {t = new Token(OP_HASH, "#", pos, line);getChar();}break;
 				case '&': {t = new Token(BIT_AMP, "&", pos, line);getChar();}break;
-				//case '~': {state = State.HAVE_EQ; getChar();}break; //BIT_XOR
+				case '~': {state = State.HAVE_XOR; getChar();}break;
 				case '|': {t = new Token(BIT_OR, "|", pos, line);getChar();}break;
-				//case '<': {state = State.HAVE_EQ; getChar();}break; //REL_LT
-				//case '>': {state = State.HAVE_EQ; getChar();}break; //REL_GT
-				//case '=': {state = State.HAVE_EQ; getChar();}break;
+				case '<': {state = State.HAVE_REL_LT; getChar();}break;
+				case '>': {state = State.HAVE_REL_GT; getChar();}break;
+				case '=': {state = State.HAVE_EQ; getChar();}break;
 				case '(': {t = new Token(LPAREN, "(", pos, line);getChar();}break;
 				case ')': {t = new Token(RPAREN, ")", pos, line);getChar();}break;
 				case '{': {t = new Token(LCURLY, "{", pos, line);getChar();}break;
 				case '}': {t = new Token(RCURLY, "}", pos, line);getChar();}break;
 				case '[': {t = new Token(LSQUARE, "[", pos, line);getChar();}break;
 				case ']': {t = new Token(RSQUARE, "]", pos, line);getChar();}break;
-				//case ':': {state = State.HAVE_EQ; getChar();}break;COLON
+				case ':': {state = State.HAVE_COLON; getChar();}break;
 				case ';': {t = new Token(SEMI, ";", pos, line);getChar();}break;
 				case ',': {t = new Token(COMMA, ",", pos, line);getChar();}break;
-				//case '.': {state = State.HAVE_EQ; getChar();}break;//DOT
+				case '.': {state = State.HAVE_DOT; sb.append("."); getChar();}break;//DOT
 
 				case '0': {t = new Token(INTLIT,"0",pos,line);getChar();}break;
-				case  -1: {t = new Token(EOF, "EOF", pos, line); 
-				//throw new LexicalException("Useful error message");
-				break;
+				case  -1: {t = new Token(EOF, "EOF", pos, line);break;
 				}
 				default: {
 					if (Character.isDigit(ch)) {
@@ -122,10 +119,80 @@ public class Scanner {
 				}
 				} // switch (ch)
 			} break;      // case START
-
-			case HAVE_EQ: {
-
+			
+			case HAVE_DIV: {
+				if(ch == '/') {
+					t = new Token(OP_DIVDIV, "//", pos, line);
+					getChar();
+				}else {
+					t = new Token(OP_DIV, "/", pos, line);
+				}
 			} break;
+			
+			case HAVE_XOR: {
+				if(ch == '=') {
+					t = new Token(REL_NOTEQ, "=~", pos, line);
+					getChar();
+				}else {
+					t = new Token(BIT_XOR, "~", pos, line);
+				}
+			} break;
+			
+			case HAVE_REL_LT: {
+				if(ch == '<') {
+					t = new Token(BIT_SHIFTL, "<<", pos, line);
+					getChar();
+				}else if(ch == '=') {
+					t = new Token(REL_LE, "<=", pos, line);
+					getChar();
+				}else {
+					t = new Token(REL_LT, "<", pos, line);
+				}
+			} break;
+			
+			case HAVE_REL_GT: {
+				if(ch == '>') {
+					t = new Token(BIT_SHIFTR, ">>", pos, line);
+					getChar();
+				}else if(ch == '=') {
+					t = new Token(REL_GE, ">=", pos, line);
+					getChar();
+				}else {
+					t = new Token(REL_GT, ">", pos, line);
+				}
+			} break;
+			
+			case HAVE_EQ: {
+				if(ch == '=') {
+					t = new Token(REL_EQEQ, "==", pos, line);
+					getChar();
+				}else {
+					t = new Token(ASSIGN, "=", pos, line);
+				}
+			} break;
+			
+			case HAVE_COLON: {
+				if(ch == ':') {
+					t = new Token(COLONCOLON, "::", pos, line);
+					getChar();
+				}else {
+					t = new Token(COLON, ":", pos, line);
+				}
+			} break;
+			
+			case HAVE_DOT: {
+				if(ch == '.') {
+					sb.append(ch);
+					getChar();
+				}else {
+					switch (sb.toString()) {
+					case "...": t = new Token(DOTDOTDOT, "...", pos, line); break;
+					case "..": t = new Token(DOTDOT, "..", pos, line); break;
+					default: t = new Token(DOT, ".", pos, line); break;
+				}
+				}
+			} break;
+			
 			case IN_NUMLIT: {  }  break;
 			case IN_IDENT: {
 				if (Character.isJavaIdentifierPart(ch)) {
